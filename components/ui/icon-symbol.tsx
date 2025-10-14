7e812f1edac0c16938df984805e6f7bfdf98d2cb
Samuel Gomes Lima <32044157+samuelgomeslima@users.noreply.github.com>
2025-10-14 18:01:09 -0300

    fix: map catechist icon

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -20,6 +20,7 @@ const MAPPING = {
   'chevron.right': 'chevron-right',
   'circle.grid.3x3.fill': 'apps',
   'bubble.left.and.bubble.right.fill': 'forum',
+  'book.fill': 'menu-book',
 } as IconMapping;
 
 /**
