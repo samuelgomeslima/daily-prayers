c8e1963f87ffd06510fbca380102402894da1cc5
Samuel Gomes Lima <32044157+samuelgomeslima@users.noreply.github.com>
2025-11-29 16:20:52 -0300

    Use Reanimated waving animation on mobile

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -1,18 +1,40 @@
-import Animated from 'react-native-reanimated';
+import { useEffect } from 'react';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
 
 export function HelloWave() {
+  const rotation = useSharedValue(0);
+
+  useEffect(() => {
+    rotation.value = withRepeat(
+      withSequence(
+        withTiming(25, { duration: 150 }),
+        withTiming(0, { duration: 150 })
+      ),
+      4,
+      false
+    );
+  }, [rotation]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotation.value}deg` }],
+  }));
+
   return (
     <Animated.Text
-      style={{
-        fontSize: 28,
-        lineHeight: 32,
-        marginTop: -6,
-        animationName: {
-          '50%': { transform: [{ rotate: '25deg' }] },
+      style={[
+        {
+          fontSize: 28,
+          lineHeight: 32,
+          marginTop: -6,
         },
-        animationIterationCount: 4,
-        animationDuration: '300ms',
-      }}>
+        animatedStyle,
+      ]}>
       👋
     </Animated.Text>
   );
